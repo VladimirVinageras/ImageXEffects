09bd6f9295cd5ac556d806736663ad3077e453f5
Vladimir Vinakheras <vvinageras@gmail.com>
2024-01-24 00:31:09 +0200

    Initial Commit

---
+++ ImageXEffects/ImageXEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageXEffectsApp.swift
+//  ImageXEffects
+//
+//  Created by Vladimir Vinakheras on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageXEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
